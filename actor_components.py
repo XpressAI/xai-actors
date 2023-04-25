375470575c6efcdf7777a4ba85e77791aee28c69
Eduardo Gonzalez <wm.eddie@gmail.com>
2023-04-26 03:03:59 +0900

    Add Actor code

---
+++ actor_components.py
@@ -0,0 +1,81 @@
+from xai_components.base import InArg, OutArg, InCompArg, Component, BaseComponent, xai_component
+import openai
+import os
+import requests
+import shutil
+from thespian.actors import ActorSystem, Actor
+
+
+@xai_component
+class CreateActorsystem(Component):
+    def execute(self, ctx) -> None:
+        ctx['actor_system'] = ActorSystem()
+        
+
+@xai_component
+class DefineActor(Component):
+    on_message: BaseComponent
+    
+    name: InCompArg[str]
+    actor_ref: OutArg[Actor]
+    message: OutArg[any]
+    sender: OutArg[Actor]
+    
+    def execute(self, ctx) -> None:
+        class XircuitsActor(Actor):
+            def receiveMessage(me, message, sender):
+                self.message.value = message
+                self.sender.value = sender
+                
+                is_done, next_body = self.on_message.do(ctx)
+                
+                while next_body:
+                    is_done, next_body = next_body.do(ctx)
+
+        system = ctx['actor_system']
+        ref = system.createActor(XircuitsActor, self.name.value)
+        
+        self.actor_ref.value = ref
+
+@xai_component
+class TellActor(Component):
+    actor_ref: InCompArg[Actor]
+    message: InCompArg[any]
+    
+    def execute(self, ctx) -> None:
+        system = ctx['actor_system']
+        system.tell(self.actor_ref.value, self.message.value)
+
+@xai_component
+class AskActor(Component):
+    actor_ref: InCompArg[Actor]
+    message: InCompArg[str]
+    wait_time: InArg[int]
+    response: OutArg[str]
+    
+    def execute(self, ctx) -> None:
+        system = ctx['actor_system']
+        self.response.value = system.ask(self.actor_ref.value, self.message.value, self.wait_time.value)
+
+
+
+@xai_component
+class SetDictValue(Component):
+    obj: InArg[dict]
+    name: InCompArg[str]
+    value: InCompArg[any]
+    out_dict: OutArg[dict]
+    
+    def execute(self, ctx) -> None:
+        obj = self.obj.value if self.obj.value else {}
+        obj[self.name.value] = self.value.value
+        self.out_dict.value = obj
+
+@xai_component
+class GetDictValue(Component):
+    obj: InCompArg[dict]
+    name: InCompArg[str]
+    value: OutArg[any]
+    
+    def execute(self, ctx) -> None:
+        self.value.value = self.obj.value[self.name.value]
